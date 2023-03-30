2761917f6b63b1723f6269ea17cc14c6ed610a5c
MrEpoch <alexandrstencuk@seznam.cz>
2023-03-30 17:17:49 +0200

    I will start to make more stable design, i am for first time testing buttons which will fill with liquid

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
       <div className="confident-text">
         <h2>We are PriTech, <span>experts around technologies</span></h2>
         <p>In our shop you can find items ranging from graphic cards and motherboards to battery chargers</p>
-        <Link to="/shop" ><button className="toShop">Browse shop</button></Link>
+        <Link className='browse-a' to="/shop" ><button className="toShop">Browse shop</button></Link>
       </div>
     </div>
   )
